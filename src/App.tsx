35fc8009a6fd3236acdbe712f92c591958d0b130
Bence Lovas <bansoul1@gmail.com>
2021-04-13 00:27:50 +0200

    diagonal grid poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement } from 'react';
+
+
+const scale = 30;
+const widthToHeight = 1.2
+
+const table = Array.from(Array(30), () => Array(30).fill(null))
+const createGrid = (table: any) => {
+  const grid = [];
+  for (let i = 0; i < table.length; i++) {
+    for (let j = 0; j < table[0].length; j++) {
+      grid.push(createDiamond(((i * scale * widthToHeight) / 2) + ((j * scale * widthToHeight) / 2), (i * scale / 2) + (-(j * scale) / 2), i, j))
+    }
+  }
+  console.log(grid)
+  return grid;
+};
+
+const createDiamond = (x: number, y: number, i: number, j: number) => (
+  <svg x={x} y={y} xmlns="http://www.w3.org/2000/svg">
+    <path 
+      fill='transparent'
+      stroke='#111'
+      strokeWidth='.21'
+      d={`  
+        M 0 ${scale / 2} 
+        L ${scale * widthToHeight / 2} 0 
+        L ${scale * widthToHeight} ${scale / 2}
+        L ${scale * widthToHeight / 2} ${scale}
+        Z`
+      }
+      onClick={() => console.log(i, j)}
+    />
+</svg>
+)
 
 function App() {
+  const width = scale * table.length;
+  const height = scale * table[0].length;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <svg
+        width={width * 2}
+        height={height * 2}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g transform="translate(0, 300)">
+          {createGrid(table)}
+
+        </g>
+        {/* {diamond} */}
+        {/* <g x={0} y={0} transform="translate(250, 0) rotate(60)" >
+          {createGrid(table)}
+        </g>
+        <g x={0} y={0} transform="translate(0, 250) rotate(-60)">
+          {createGrid(table)}
+        </g> */}
+      </svg>
     </div>
   );
 }
