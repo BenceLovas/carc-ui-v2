567f77cd2fc5bc5ed7bc9584f1914d71aaeb9c0f
Bence Lovas <bansoul1@gmail.com>
2021-04-17 23:50:59 +0200

    on hover show the tile to be placed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,53 @@
 import React, { useEffect, useState } from "react";
 
-const scale = 50;
+export const baseTileCounts: Map<string, number> = new Map([
+  ["chapel_with_road", 2],
+  ["chapel", 4],
+  ["full_city_with_shield", 1],
+  ["city_top_straight_road", 3],
+  ["city_top", 5],
+  ["city_narrow_shield", 2],
+  ["city_narrow", 1],
+  ["city_left_right", 3],
+  ["city_top_right", 2],
+  ["city_top_road_bend_right", 3],
+  ["city_top_road_bend_left", 3],
+  ["city_top_crossroads", 3],
+  ["city_diagonal_top_right_shield", 2],
+  ["city_diagonal_top_right", 3],
+  ["city_diagonal_top_left_shield_road", 2],
+  ["city_diagonal_top_left_road", 3],
+  ["city_bottom_grass_shield", 1],
+  ["city_bottom_grass", 3],
+  ["city_bottom_road_shield", 2],
+  ["city_bottom_road", 1],
+  ["straight_road", 8],
+  ["bent_road", 9],
+  ["three_split_road", 4],
+  ["crossroads", 1],
+]);
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray<T>(array: T[]): void {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function initializeDeck() {
+  const tempDeck: string[] = [];
+  for (const [cardName, count] of baseTileCounts.entries()) {
+    for (let i = 0; i < count; i++) {
+      tempDeck.push(cardName);
+    }
+  }
+  shuffleArray(tempDeck);
+
+  return tempDeck;
+}
+
+const scale = 100;
 const widthToHeight = 1.2;
 const table = Array.from(Array(30), () => Array(30).fill(null));
 
@@ -9,33 +56,9 @@ function App() {
   const height = scale * table[0].length;
   const [tiles, setTiles] = useState<any>([]);
   const [rotation, setRotation] = useState<number>(0);
-  const [cards, setCards] = useState<any>([
-    "chapel_with_road",
-    "chapel",
-    "full_city_with_shield",
-    "city_top_straight_road",
-    "city_top",
-    "city_narrow_shield",
-    "city_narrow",
-    "city_left_right",
-    "city_top_right",
-    "city_top_road_bend_right",
-    "city_top_road_bend_left",
-    "city_top_crossroads",
-    "city_diagonal_top_right_shield",
-    "city_diagonal_top_right",
-    "city_diagonal_top_left_shield_road",
-    "city_diagonal_top_left_road",
-    "city_bottom_grass_shield",
-    "city_bottom_grass",
-    "city_bottom_road_shield",
-    "city_bottom_road",
-    "straight_road",
-    "bent_road",
-    "three_split_road",
-    "crossroads",
-  ])
-  const [currentCard, setCurrentCard] = useState<number>(0)
+  const [cards, setCards] = useState<any>(initializeDeck());
+  const [currentCard, setCurrentCard] = useState<number>(0);
+  const [hoveredCoordinate, setHoveredCoordinate] = useState<any>(null);
   useEffect(() => {
     document.addEventListener("keydown", keyDownHandler, false);
     return () => {
@@ -78,7 +101,14 @@ function App() {
     return grid;
   };
 
-  const createDiamond = (x: number, y: number, i: number, j: number, stroke: string, isInteractive: boolean) => (
+  const createDiamond = (
+    x: number,
+    y: number,
+    i: number,
+    j: number,
+    stroke: string,
+    isInteractive: boolean
+  ) => (
     <svg x={x} y={y} xmlns="http://www.w3.org/2000/svg">
       <path
         fill="transparent"
@@ -93,19 +123,29 @@ function App() {
         onClick={() => {
           if (!isInteractive) return;
           setTiles(() => {
-            const newTiles = [...tiles, { row: i, column: j, rotation, choosenTile: cards[currentCard] }].sort((a, b) => {
-              if (a.row < b.row){
+            const newTiles = [
+              ...tiles,
+              { row: i, column: j, rotation, choosenTile: cards[currentCard] },
+            ].sort((a, b) => {
+              if (a.row < b.row) {
                 return -1;
               }
-              if (a.row > b.row){
+              if (a.row > b.row) {
                 return 1;
               }
               return a.column > b.column ? -1 : 1;
-            })
+            });
             setCurrentCard(currentCard + 1);
             return newTiles;
           });
-          setRotation(0)
+          setRotation(0);
+          setHoveredCoordinate(null);
+        }}
+        onMouseOver={() => {
+          if (!isInteractive) return;
+          console.log("hovered");
+          console.log({ x, y });
+          setHoveredCoordinate({ x, y });
         }}
       />
     </svg>
@@ -113,15 +153,15 @@ function App() {
 
   const createTiles = (tiles: any): any => {
     return tiles.map((tile: any) => {
-      
       return (
         <svg
           key={`${tile.row}_${tile.column}`}
           x={
             (tile.row * scale * widthToHeight) / 2 +
-            (tile.column * scale * widthToHeight) / 2 - (scale / 40)
+            (tile.column * scale * widthToHeight) / 2 -
+            scale / 40
           }
-          y={(tile.row * scale) / 2 + -(tile.column * scale) / 2 - (scale / 2.6)}
+          y={(tile.row * scale) / 2 + -(tile.column * scale) / 2 - scale / 2.6}
           xmlns="http://www.w3.org/2000/svg"
         >
           <image
@@ -141,9 +181,25 @@ function App() {
         height={height * 2}
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g>{createGrid(table, '#111', false)}</g>
+        <g>{createGrid(table, "#111", false)}</g>
         <g>{createTiles(tiles)}</g>
-        <g>{createGrid(table, 'transparent', true)}</g>
+        {hoveredCoordinate && (
+          <svg
+            key={`${hoveredCoordinate.x}_${hoveredCoordinate.y}`}
+            x={hoveredCoordinate.x - scale / 40}
+            y={hoveredCoordinate.y - scale / 2.6}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <image
+              href={`assets/images/tiles_${cards[currentCard]}_${rotation}.svg`}
+              height={scale * widthToHeight}
+              width={scale}
+              transform={`scale(1.25 1.27)`}
+              opacity="0.5"
+            />
+          </svg>
+        )}
+        <g>{createGrid(table, "transparent", true)}</g>
       </svg>
     </div>
   );
